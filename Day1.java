71796c63134e1812d2e4b8b77ba547cf9930cf34
Eunike Dukkun <edukkun@gmail.com>
2023-10-09 19:49:24 +0800

    Day 1

---
+++ Day1.java
@@ -0,0 +1,22 @@
+package ISC100days;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+
+        String nama = "Eunike Dukkun";
+        long nim = 0221354;
+        int umur = 20;
+        double TinggiBadan = 1.52;
+        char jenisKelamin = 'P';
+        boolean sudahMenikah = false;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM : D" + nim);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi Badan: " + TinggiBadan);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Sudah Menikah: " + sudahMenikah);
+
+    }
+}

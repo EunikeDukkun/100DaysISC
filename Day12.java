5fc9bb201fa8f92f459c6e9e33282751088dbf72
Eunike Dukkun <edukkun@gmail.com>
2023-10-20 23:53:36 +0800

    perulangan

---
+++ Day12.java
@@ -0,0 +1,14 @@
+package ISC100days;
+
+public class Day12 {
+
+    public static void main(String[] args) {
+        int[] numbers = {8, 1, 7, 2, 10, 6, 4, 3, 9, 5};
+        int i = 0;
+        while (i < numbers.length) {
+            System.out.println("number: " + numbers[i]);
+            i++;
+        }
+    }
+
+}

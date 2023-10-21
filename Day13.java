d6982bf4f780bb36f74423b385a2d9936eba7864
Eunike Dukkun <edukkun@gmail.com>
2023-10-21 22:56:30 +0800

    Percabangan

---
+++ Day13.java
@@ -0,0 +1,17 @@
+package ISC100days;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        String warna = "merah";
+
+        if (warna.equals("merah")) {
+            System.out.println("BERHENTI");
+        } else if (warna.equals("kuning")) {
+            System.out.println("HATI-HATI");
+        } else if (warna.equals("hijau")) {
+            System.out.println("JALAN");
+        }
+    }
+
+}

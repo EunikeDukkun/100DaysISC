530a5f67d659c65b32dc0fd7a9208c03d5bda478
Eunike Dukkun <edukkun@gmail.com>
2023-10-23 23:33:39 +0800

    Perulangan for

---
+++ Day15.java
@@ -0,0 +1,16 @@
+
+package ISC100days;
+
+public class Day15 {
+    
+
+  public static void main(String[] args) {
+    // Mencetak angka dari 1 hingga 10
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+  }
+}
+
+    
+

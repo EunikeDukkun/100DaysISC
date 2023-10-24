3a0a3cc3e9ce665e35ea9f9215e3a962d757c353
Eunike Dukkun <edukkun@gmail.com>
2023-10-24 23:45:56 +0800

    perulangan

--- Day15.java
+++ Day15.java
@@ -1,16 +1,11 @@
-
 package ISC100days;
 
 public class Day15 {
-    
 
-  public static void main(String[] args) {
-    // Mencetak angka dari 1 hingga 10
-    for (int i = 1; i <= 10; i++) {
-      System.out.println(i);
+    public static void main(String[] args) {
+        // Mencetak angka dari 1 hingga 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
-  }
 }
-
-    
-

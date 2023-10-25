f01dafcbc3d677b0b81cde83ec86d334686bdb6b
Eunike Dukkun <edukkun@gmail.com>
2023-10-25 23:22:04 +0800

    perulangan

---
+++ Day17.java
@@ -0,0 +1,11 @@
+package ISC100days;
+
+public class Day17 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i = i + 2) {
+            System.out.print(i + ", ");
+        }
+    }
+
+}

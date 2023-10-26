377a7073e637aedf1cd5a3acf509dcabe4654a93
Eunike Dukkun <edukkun@gmail.com>
2023-10-26 22:51:50 +0800

    perulangan

---
+++ Day18.java
@@ -0,0 +1,21 @@
+package ISC100days;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        int i;
+        for (i = 0; i < 4; i++) {
+            if (i >= 3) {
+                break;
+            }
+            System.out.println("Selamat Belajar!");
+            if (i >= 2) {
+                continue;
+            }
+            System.out.println("JAVA");
+        }
+        System.out.println(i);
+
+    }
+
+}

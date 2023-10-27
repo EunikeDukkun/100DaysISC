5b4bfc37ef6f60ab2252313cac022bbe871a5d1b
Eunike Dukkun <edukkun@gmail.com>
2023-10-27 22:06:55 +0800

    perulangan

---
+++ day19.java
@@ -0,0 +1,19 @@
+package ISC100days;
+
+public class day19 {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <= 10) {
+            // blok kode yang akan diulang
+            System.out.println(
+                    "Perulangan ke-" + i
+            );
+
+            // increment nilai i
+            i++;
+        }
+    }
+
+}

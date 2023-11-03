d8227591fb20e20bf2c1492810446959cb7c48a2
Eunike Dukkun <edukkun@gmail.com>
2023-11-03 22:20:32 +0800

    penggunaan break

---
+++ Day26.java
@@ -0,0 +1,15 @@
+package ISC100days;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+        //Penggunaan Break
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+
+}

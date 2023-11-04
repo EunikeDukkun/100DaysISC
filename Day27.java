d6129a898e0f1e34624eeec141408d795ca465e3
Eunike Dukkun <edukkun@gmail.com>
2023-11-04 19:27:29 +0800

    penggunaan continue

---
+++ Day27.java
@@ -0,0 +1,14 @@
+package ISC100days;
+
+public class Day27 {
+
+    public static void main(String[] args) {
+        //Penggunaan Continue
+        for (int i = 0; i <= 15; i++) {
+            if (i == 4) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}

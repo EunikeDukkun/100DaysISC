8a9fc51fa40089fd16d4de57c14aaeba359afb81
Eunike Dukkun <edukkun@gmail.com>
2023-11-05 21:24:10 +0800

    penggunaakan Array

---
+++ Day28.java
@@ -0,0 +1,12 @@
+package ISC100days;
+
+public class Day28 {
+
+    public static void main(String[] args) {
+        //menggunakan Array
+        String[] buah = {"Apukat", "Pisang", "Anggur", "Rambutan", "Apel"};
+        buah[0] = "Pisang";
+        System.out.println(buah[0]);
+    }
+
+}

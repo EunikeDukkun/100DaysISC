2a7bbb393f66e057e87d2c045082fdef86193cab
Eunike Dukkun <edukknun@gmail.com>
2023-11-16 23:35:38 +0800

    array menampung 10 nama hewan

---
+++ Day39.java
@@ -0,0 +1,14 @@
+package ISC100days;
+
+public class Day39 {
+
+    public static void main(String[] args) {
+
+        String[] namaHewan = {"Singa", "Gajah", "Harimau", "Jerapah", "Kanguru", "Kuda", "Burung Kakatua", "Koala", "Buaya", "Ular"};
+
+        for (int i = 0; i < namaHewan.length; i++) {
+            System.out.println("Hewan ke-" + (i + 1) + ": " + namaHewan[i]);
+        }
+    }
+
+}

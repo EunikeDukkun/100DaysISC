227cbed9a754a2e67aecf904ec0b24a2f8dce9e7
EunikeDukkun <100480072+EunikeDukkun@users.noreply.github.com>
2023-11-22 18:24:12 +0800

    Array List

---
+++ Day45.java
@@ -0,0 +1,32 @@
+package ISC100days;
+
+public class Day45 {
+
+    public static void main(String[] args) {
+        int myBias = 2;
+        switch (myBias) {
+            case 1:
+                System.out.println("Haechan");
+                break;
+            case 2:
+                System.out.println("Mark Lee");
+                break;
+            case 3:
+                System.out.println("Jisung");
+                break;
+            case 4:
+                System.out.println("Renjun");
+                break;
+            case 5:
+                System.out.println("Jaemin");
+                break;
+            case 6:
+                System.out.println("Jeno");
+                break;
+            case 7:
+                System.out.println("Chenlee");
+                break;
+        }
+    }
+
+}

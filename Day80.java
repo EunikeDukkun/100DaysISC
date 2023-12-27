2ce409eb3bcaf1a206e529f649f4166ab34e5cf2
EunikeDukkun <100480072+EunikeDukkun@users.noreply.github.com>
2023-12-27 21:52:40 +0800

    percabangan intuk  hasil bagi

---
+++ Day80.java
@@ -0,0 +1,34 @@
+
+package ISC100days;
+
+import java.util.Scanner;
+
+public class Day80 {
+    public static void main(String[] args) {
+        //Program Scanner untuk menggunakan inputan eser
+        Scanner sc = new Scanner(System.in);
+        //program untuk menampilkan pesan memasukkan nilai x
+        System.out.println("Masukkan nilai x: ");
+        //membuat variabel x yang bertipe integer untuk menyimpan nilai yang di inputkan oleh user
+        int x = sc.nextInt();
+        System.out.println("Masukkan nilai y: ");
+        int y = sc.nextInt();
+        //membuat variabel kosong(y) bertipe integer untuk menyimpan hasil penju]mlahan nantnya
+        int z=0;
+        //variabel yang menyimpan rumus
+        int a = x + y;
+        //percabangan 
+        if (a >= 4) {
+            z = x*y;
+            System.out.println(z);
+        }
+        if (a ==3) {
+            z = a-y;
+            System.out.println(z);
+        }
+        System.out.println(z2
+        4);
+    
+    }
+    
+}
